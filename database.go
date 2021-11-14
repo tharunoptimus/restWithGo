64285e9ce8381cc4ec0ad86cae9ec284c68eb2d2
tharunoptimus <tharunoptimus@outlook.com>
2021-11-14 17:30:38 +0530

    Added ping function to check the connection

--- database.go
+++ database.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
@@ -17,4 +19,22 @@ func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, er
 	// mongo.Connect return mongo.Client method
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	return client, ctx, cancel, err
+}
+
+// This is a user defined method that accepts
+// mongo.Client and context.Context
+// This method used to ping the mongoDB, return error if any.
+
+func ping(client *mongo.Client, ctx context.Context) error {
+
+	// mongo.Client has Ping to ping mongoDB, deadline of
+	// the Ping method will be determined by cxt
+	// Ping method return error if any occurred, then
+	// the error can be handled.
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("Database Connection Successful")
+	return nil
 }
