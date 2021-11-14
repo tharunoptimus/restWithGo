532db3051da5fe3fbddaac73de58ceff3dab5c56
tharunoptimus <tharunoptimus@outlook.com>
2021-11-14 17:29:06 +0530

    Added connect func to connect to mongodb

---
+++ database.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
+
+	// ctx will be used to set deadline for process, here
+	// deadline will of 30 seconds.
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+
+	// mongo.Connect return mongo.Client method
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	return client, ctx, cancel, err
+}
