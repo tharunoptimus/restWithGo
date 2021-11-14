6422964169f09647fd0a201667a1bc08e92d01fe
tharunoptimus <tharunoptimus@outlook.com>
2021-11-14 17:31:23 +0530

    Added close function to close the mongodb con

--- database.go
+++ database.go
@@ -10,6 +10,24 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+func close(client *mongo.Client, ctx context.Context,
+	cancel context.CancelFunc) {
+
+	// CancelFunc to cancel to context
+	defer cancel()
+
+	// client provides a method to close
+	// a mongoDB connection.
+	defer func() {
+
+		// client.Disconnect method also has deadline.
+		// returns error if any,
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
+
 func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
 
 	// ctx will be used to set deadline for process, here
